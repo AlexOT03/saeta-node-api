682d2367b913de161ec50cc4b89262d426d19363
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-11-25 10:21:58 -0600

    fix CORS error

--- index.js
+++ index.js
@@ -1,169 +1,183 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
-import fs from 'fs';
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import fs from "fs";
+import cors from "cors"; // Importa la biblioteca CORS
+import helmet from "helmet";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app = express(); 
-const PORT = process.env.PORT || 3000
+// Middleware para habilitar CORS
+app.use(
+  cors({
+    origin: "*", // Permitir cualquier origen (ajustar en producción)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Métodos HTTP permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Cabeceras permitidas
+  })
+);
+
+// Middleware para parsear JSON en las solicitudes
+app.use(express.json());
 
 const readData = () => {
-    try {
-        const data = fs.readFileSync('./db.js');
-        return JSON.parse(data);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const data = fs.readFileSync("./db.js");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const withData = (data) => {
-    try {
-        fs.withFileSync('./db.js', JSON.stringify(data));
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    fs.withFileSync("./db.js", JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-app.use(express.static(path.join(__dirname, 'public')));
- 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/api/routes', (req, res) => {
-    const data = readData();
-    res.send(data.routes);
+app.get("/api/routes", (req, res) => {
+  const data = readData();
+  res.send(data.routes);
 });
 
-app.get('/api/route/:id', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.id));
-    res.send(route);
+app.get("/api/route/:id", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.id)
+  );
+  res.send(route);
 });
 
-app.get('/api/route/:routeId/goings', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
-    res.send(route.Going);
+app.get("/api/route/:routeId/goings", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
+  res.send(route.Going);
 });
 
-app.get('/api/route/:routeId/going/:goingId', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
+app.get("/api/route/:routeId/going/:goingId", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
 
-    const going = route.Going.find(going => going.id === parseInt(req.params.goingId));
-    if (!going) return res.status(404).send('Going not found');
+  const going = route.Going.find(
+    (going) => going.id === parseInt(req.params.goingId)
+  );
+  if (!going) return res.status(404).send("Going not found");
 
-    res.send(going);
+  res.send(going);
 });
 
-app.get('/api/route/:routeId/going/:goingId/stops', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
+app.get("/api/route/:routeId/going/:goingId/stops", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
 
-    const going = route.Going.find(going => going.id === parseInt(req.params.goingId));
-    if (!going) return res.status(404).send('Going not found');
+  const going = route.Going.find(
+    (going) => going.id === parseInt(req.params.goingId)
+  );
+  if (!going) return res.status(404).send("Going not found");
 
-    res.send(going.stops);
+  res.send(going.stops);
 });
 
-app.get('/api/route/:routeId/going/:goingId/stop/:stopId', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
+app.get("/api/route/:routeId/going/:goingId/stop/:stopId", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
 
-    const going = route.Going.find(going => going.id === parseInt(req.params.goingId));
-    if (!going) return res.status(404).send('Going not found');
+  const going = route.Going.find(
+    (going) => going.id === parseInt(req.params.goingId)
+  );
+  if (!going) return res.status(404).send("Going not found");
 
-    const stop = going.stops.find(stop => stop.id === parseInt(req.params.stopId));
-    if (!stop) return res.status(404).send('Stop not found');
+  const stop = going.stops.find(
+    (stop) => stop.id === parseInt(req.params.stopId)
+  );
+  if (!stop) return res.status(404).send("Stop not found");
 
-    res.send(stop);
+  res.send(stop);
 });
 
-
-app.get('/api/route/:routeId/returns', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
-    res.send(route.Return);
+app.get("/api/route/:routeId/returns", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
+  res.send(route.Return);
 });
 
-app.get('/api/route/:routeId/return/:returnId', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
+app.get("/api/route/:routeId/return/:returnId", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
 
-    const returnRoute = route.Return.find(returnRoute => returnRoute.id === parseInt(req.params.returnId));
-    if (!returnRoute) return res.status(404).send('Return route not found');
+  const returnRoute = route.Return.find(
+    (returnRoute) => returnRoute.id === parseInt(req.params.returnId)
+  );
+  if (!returnRoute) return res.status(404).send("Return route not found");
 
-    res.send(returnRoute);
+  res.send(returnRoute);
 });
 
-app.get('/api/route/:routeId/return/:returnId/stops', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
+app.get("/api/route/:routeId/return/:returnId/stops", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
 
-    const returnRoute = route.Return.find(returnRoute => returnRoute.id === parseInt(req.params.returnId));
-    if (!returnRoute) return res.status(404).send('Return route not found');
+  const returnRoute = route.Return.find(
+    (returnRoute) => returnRoute.id === parseInt(req.params.returnId)
+  );
+  if (!returnRoute) return res.status(404).send("Return route not found");
 
-    res.send(returnRoute.stops);
+  res.send(returnRoute.stops);
 });
 
-app.get('/api/route/:routeId/return/:returnId/stop/:stopId', (req, res) => {
-    const data = readData();
-    const route = data.routes.find(route => route.id === parseInt(req.params.routeId));
-    if (!route) return res.status(404).send('Route not found');
+app.get("/api/route/:routeId/return/:returnId/stop/:stopId", (req, res) => {
+  const data = readData();
+  const route = data.routes.find(
+    (route) => route.id === parseInt(req.params.routeId)
+  );
+  if (!route) return res.status(404).send("Route not found");
 
-    const returnRoute = route.Return.find(returnRoute => returnRoute.id === parseInt(req.params.returnId));
-    if (!returnRoute) return res.status(404).send('Return route not found');
+  const returnRoute = route.Return.find(
+    (returnRoute) => returnRoute.id === parseInt(req.params.returnId)
+  );
+  if (!returnRoute) return res.status(404).send("Return route not found");
 
-    const stop = returnRoute.stops.find(stop => stop.id === parseInt(req.params.stopId));
-    if (!stop) return res.status(404).send('Stop not found');
+  const stop = returnRoute.stops.find(
+    (stop) => stop.id === parseInt(req.params.stopId)
+  );
+  if (!stop) return res.status(404).send("Stop not found");
 
-    res.send(stop);
-});
-
-app.post('/api/route', (req, res) => {
-    const data = readData();
-    const body = req.body;
-    const newRoute = {
-        id: data.routes.length + 1,
-        ...body
-    }
-    data.routes.push(newRoute);
-    withData(data);
-});
-
-app.put('/api/route/:id', (req, res) => {
-    const data = readData();
-    const body = req.body;
-    const id = parseInt(req.params.id);
-    const routeIndex = data.routes.findIndex(route => route.id === id);
-    data.routes[routeIndex] = {
-        ...data.routes[routeIndex],
-        ...body
-    }
-    withData(data);
-    res.JSON({message: 'Route updated'});
-});
-
-app.delete('/api/route/:id', (req, res) => {	
-    const data = readData();
-    const id = parseInt(req.params.id);
-    const routeIndex = data.routes.findIndex(route => route.id === id);
-    data.routes.splice(routeIndex, 1);
-    withData(data);
-    res.JSON({message: 'Route deleted'});
+  res.send(stop);
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
